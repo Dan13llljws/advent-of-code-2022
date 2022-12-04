07c2095c3129282cf9d184a7752343802dcea847
Dan13llljws <dan13jws@gmail.com>
2022-12-04 00:11:29 -0500

    day 4

---
+++ day4p2.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    ifstream ifs("input.txt");
+    string s;
+    int ans = 0;
+    while(getline(ifs, s)){
+        vector<int> a;
+        int x = 0;
+        for (char c : s){
+            if (isdigit(c)){
+                x = x * 10 + c - '0';
+            } else {
+                a.push_back(x);
+                x = 0;
+            }
+        }
+        a.push_back(x);
+        if (a[1] >= a[2] && a[0] <= a[3]) ans++;
+        else if (a[1] <= a[2] && a[0] >= a[3]) ans++;
+    }
+    cout << ans << '\n';
+}
