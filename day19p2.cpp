3c22ff872172e6c3f4ba569769c68d533146bf75
Dan13llljws <dan13jws@gmail.com>
2022-12-19 20:53:04 -0500

    菊花残，满地伤你的笑容已泛黄

---
+++ day19p2.cpp
@@ -0,0 +1,51 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+
+map<tuple<int,int,int>,int> dp[33][5][21][21][33];
+int oc, cc, oco, occ, gco, gcb;
+int dfs(int t, int o, int c, int ob, int ro, int rc, int rob, int rg){
+    if (t == 32) return 0;
+    if (ro > max({oc, cc, oco, gco}) || rc > occ || rob > gcb) return 0;
+    if (o >= 30) o = 30;
+    if (c >= 40) c = 40;
+    if (dp[t][ro][rc][rob][rg].count({o, c, ob})) return dp[t][ro][rc][rob][rg][{o, c, ob}];
+    int ret = 0;
+    ret = max(ret, dfs(t + 1, o + ro, c + rc, ob + rob, ro, rc, rob, rg));
+    if (o >= oc) ret = max(ret, dfs(t + 1, o + ro - oc, c + rc, ob + rob, ro + 1, rc, rob, rg));
+    if (o >= cc) ret = max(ret, dfs(t + 1, o + ro - cc, c + rc, ob + rob, ro, rc + 1, rob, rg));
+    if (o >= oco && c >= occ) 
+        ret = max(ret, dfs(t + 1, o + ro - oco, c + rc - occ, ob + rob, ro, rc, rob + 1, rg));
+    if (o >= gco && ob >= gcb) 
+        ret = max(ret, dfs(t + 1, o + ro - gco, c + rc, ob + rob - gcb, ro, rc, rob, rg + 1));
+    dp[t][ro][rc][rob][rg][{o, c, ob}] = ret + rg;
+    return ret + rg;
+}
+int main(){
+    ifstream ifs("in");
+    string s;
+    auto read = [&](int &x, istringstream& iss, int k){
+        string buf;
+        for (int i = 0; i < k; i++) iss >> buf;
+        iss >> x;
+    };
+    int ans = 1;
+    while(getline(ifs, s)){
+        istringstream iss(s);
+        read(oc, iss, 6);
+        read(cc, iss, 5);
+        read(oco, iss, 5);
+        read(occ, iss, 2);
+        read(gco, iss, 5);
+        read(gcb, iss, 2);
+        for (int i = 0; i < 32; i++)
+            for (int j = 0; j < 5; j++)
+                for (int k = 0; k < 21; k++)
+                    for (int l = 0; l < 21; l++)
+                        for (int m = 0; m < 32; m++)
+                            dp[i][j][k][l][m].clear();
+        cout << dfs(0, 0, 0, 0, 1, 0, 0, 0) << endl;
+        ans *= dfs(0, 0, 0, 0, 1, 0, 0, 0);
+    }
+    cout << ans << endl;
+}
